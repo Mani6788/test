cf9dd7e437f32bb9912b1bd57ba80c8d2e1248d3
Mani <114245506+Mani6788@users.noreply.github.com>
2022-10-01 19:47:04 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,6 @@ streamlit.text('kale ,spinach & rocket smoothie')
 streamlit.text('hard boiled freerange egg') 
 
 streamlit.header("Build you own fruit smoothie")
+import pandas
+my_fruits_list =pandas.read_csv( "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruits_list)
